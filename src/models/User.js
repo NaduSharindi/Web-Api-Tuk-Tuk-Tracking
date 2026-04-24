49949a2bf57a0fbd953ab8ebc8e3e4e69a3d8a12
Nadunika Sharindi <84509044+NaduSharindi@users.noreply.github.com>
2026-04-24 14:12:48 +0530

    fix: update mongoose pre-save hook for password hashing

--- src/models/User.js
+++ src/models/User.js
@@ -8,12 +8,11 @@ const userSchema = new mongoose.Schema({
     stationId: { type: mongoose.Schema.Types.ObjectId, ref: 'PoliceStation' } // Optional, for station officers
 }, { timestamps: true });
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+// Hash password before saving (Modern Mongoose version)
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 export default mongoose.model('User', userSchema);
