cfa82d749ebc1c91f1edd8b73a67ef63a7b4600d
Nadunika Sharindi <84509044+NaduSharindi@users.noreply.github.com>
2026-04-24 12:55:12 +0530

    feat: implement database models for regions-district

---
+++ src/models/District.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const districtSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    provinceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Province', required: true }
+}, { timestamps: true });
+
+export default mongoose.model('District', districtSchema);
